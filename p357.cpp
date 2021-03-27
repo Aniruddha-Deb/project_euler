0fbbb5daf4b4cb3c3bf20a6fe6341e28a3a97b4c
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-03-27 17:07:59 +0530

    Tried solving #357

---
+++ p357.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <bitset>
+
+using namespace std;
+
+#define N 100000000
+
+bool is_prime(int n) {
+	for (int i=2; i<(int)(sqrt(n)+1); i++) {
+		if (n%i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+
+	cout << "started" << endl;
+	
+	bitset<N+5> *A = new bitset<N+5>();
+
+	for (int i=2; i*i <= N; i++) {
+		for (int j=1; i*i*j <= N; j++) {
+			A->set(i*i*j);
+		}
+	}
+
+	long sum = 0;
+
+	for (int i=2; i <= N; i++) {
+		if ((*A)[i] == false && i%2 == 0 && is_prime(i+1)) {
+			sum += i;
+		}
+	}
+
+	cout << sum << endl;
+
+	delete A;
+	return 0;
+}
---
+++ p357.py
@@ -0,0 +1,14 @@
+plist = []
+
+def is_prime(n):
+    for i in list:
+        if n%i == 0:
+            return False
+    return True
+
+def gen_primes():
+    for i in range(2,100000000/2+1):
+        if is_prime(i):
+            plist.append(i)
+
+
